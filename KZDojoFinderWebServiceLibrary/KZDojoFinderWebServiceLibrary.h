11bcc78281750d9803e36bfec4d4a14a8496f5e4
Shane Whitehead <obishiro@gmail.com>
2016-04-20 11:26:20 +1000

    Initial Commit

---
+++ KZDojoFinderWebServiceLibrary/KZDojoFinderWebServiceLibrary.h
@@ -0,0 +1,19 @@
+//
+//  KZDojoFinderWebServiceLibrary.h
+//  KZDojoFinderWebServiceLibrary
+//
+//  Created by Shane Whitehead on 20/04/2016.
+//  Copyright © 2016 KaiZen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KZDojoFinderWebServiceLibrary.
+FOUNDATION_EXPORT double KZDojoFinderWebServiceLibraryVersionNumber;
+
+//! Project version string for KZDojoFinderWebServiceLibrary.
+FOUNDATION_EXPORT const unsigned char KZDojoFinderWebServiceLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KZDojoFinderWebServiceLibrary/PublicHeader.h>
+
+
---
+++ README.md
@@ -0,0 +1,5 @@
+#KZDojoFinderWebServiceLibraryThis is an implementation of the [KZDojoFinderLibrary](https://github.com/RustyKnight/KZDojoFinderLibrary) which uses the [KZDojoFinderWebServiceClientLibrary](https://github.com/RustyKnight/KZDojoFinderWebServiceClientLibrary) to load data from a web service.
+
+This is inteneded to be a bare bones implementation designed to provide a proofing ground for the web service and associated APIs.
+
+This API provides no caching, so all data is loaded from the web service every time.
